691cfe99f6c71792845fc838d02184e30070ec7c
ASUS <804356962@qq.com>
2018-03-03 13:26:00 +0800

    修改了网络层

--- TensorFlow_Weigths_baises/BuildNeuralNetwork.py
+++ TensorFlow_Weigths_baises/BuildNeuralNetwork.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
 
 def add_layer(inputs,in_size,out_size,activation_function=None):
-    Weights=tf.Variable(tf.random_normal([in_size,out_size]))
+  # Weights=tf.Variable(tf.random_normal([in_size,out_size]))
+    #biases=
